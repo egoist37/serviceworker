05951e588f923f8ea3c6f29e2d98b647ca42a422
egoist37 <lymarst@gmail.com>
2017-07-07 00:34:21 +0300

    Update and rename ww.js to service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,14 @@
 Notification.requestPermission(function (permission) {
 // Если права успешно получены, отправляем уведомление
 if (permission === "granted") {
- var socket = new WebSocket("http://localhost:3031");
+ var socket = new WebSocket("ws://localhost:3031");
 socket.onopen = function() {
   console.log("Соединение установлено.");
 };
 socket.onmessage = function(event) {
   console.log("Получены данные " + event.data);
-new Notification('Верните Линуса!', {
-body: 'Тестирование HTML5 Notifications',
+new Notification('твою ж мать, работый!', {
+body: 'Тестирование WEBPUSH Notifications',
 icon: 'icon.jpg',
 dir: 'auto'
 });
