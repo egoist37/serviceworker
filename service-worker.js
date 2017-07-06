aca5add4ed1bd6db456a158e0546d58ade7401fd
egoist37 <lymarst@gmail.com>
2017-07-07 00:38:37 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,12 @@
-Notification.requestPermission(function (permission) {
-// Если права успешно получены, отправляем уведомление
-if (permission === "granted") {
- var socket = new WebSocket("ws://localhost:3031");
-socket.onopen = function() {
-  console.log("Соединение установлено.");
+var socket = new WebSocket("ws://localhost:3031");
+socket.onopen = function () {
+    console.log("Соединение установлено.");
 };
-socket.onmessage = function(event) {
-  console.log("Получены данные " + event.data);
-new Notification('твою ж мать, работый!', {
-body: 'Тестирование WEBPUSH Notifications',
-icon: 'icon.jpg',
-dir: 'auto'
-});
+socket.onmessage = function (event) {
+    console.log("Получены данные " + event.data);
+    new Notification('твою ж мать, работый!', {
+        body: 'Тестирование WEBPUSH Notifications',
+        icon: 'icon.jpg',
+        dir: 'auto'
+    });
 };
-} else {
-alert('Вы запретили показывать уведомления'); // Юзер отклонил наш запрос на показ уведомлений
-}
-});
